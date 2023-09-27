d4d73ec9cc98118dbfc6e414e5384420e84e3728
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-27 16:08:37 +0200

    Add bookmark_id to posts serializers.py - backend

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,8 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
-from .models import Post
 from likes.models import Like
+from bookmarks.models import Bookmark
+from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -12,6 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
+    bookmark_id = serializers.SerializerMethodField()
     comments_count = serializers.ReadOnlyField()
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
@@ -29,6 +31,7 @@ class PostSerializer(serializers.ModelSerializer):
                 'Image width larger than 4096px'
             )
         return value
+
     def get_created_at(self, obj):
         """Set DATETIME format of created_at field to naturaltime"""
         return naturaltime(obj.created_at)
@@ -53,11 +56,22 @@ class PostSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def get_bookmark_id(self, obj):
+        """Get the bookmark_id field"""
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return bookmark.id if bookmark else None
+        return None
+
     class Meta:
         """Data structuring"""
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'title', 'travel', 'summary', 'content', 'image', 'like_id',
-            'comments_count', 'likes_count', 'created_at', 'updated_at'
+            'comments_count', 'likes_count', 'created_at', 'updated_at',
+            'bookmark_id',
         ]
