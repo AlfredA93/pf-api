a0b5d6c80d90c204346ea8aa1ac2bac8161391dd
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-28 16:03:04 +0200

    Edit CORS settings.py - Backend

--- photofootapi/settings.py
+++ photofootapi/settings.py
@@ -116,8 +116,10 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get('CLIENT_ORIGIN')
+    os.environ.get('CLIENT_ORIGIN'),
 ]
+
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
