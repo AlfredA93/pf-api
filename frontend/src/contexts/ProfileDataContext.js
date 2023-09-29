77b877a84dfde072af94034f8f2c630391cbad20
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-29 14:14:15 +0200

    Cut PopularProfiles logic to ProfileDataContext

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -1,7 +1,41 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "./CurrentUserContext";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
 
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = () => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] }, // We will use this variable later
+    popularProfiles: { results: [] },
+  });
+
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/api/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [currentUser]);
+
+  return (
+
+  <div>ProfileDataProvider</div>
+  
+  );
+};
