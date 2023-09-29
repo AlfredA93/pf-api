fa83da9750ef07fcda0c036b3cbc234103a27094
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-29 10:30:17 +0200

    Add MoreDropdown   edit/delete component - Comment

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -3,9 +3,15 @@ import styles from "../../styles/Comment.module.css";
 import { Media } from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 import { Link } from "react-router-dom/cjs/react-router-dom";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
+
+    const currentUser = useCurrentUser()
+    const is_owner = currentUser?.username === owner;
+
   return (
     <div>
       <hr />
@@ -20,6 +26,7 @@ const Comment = (props) => {
         <span className={styles.Date}>{updated_at}</span>
         <p>{content}</p>
       </Media.Body>
+      {is_owner && (<MoreDropdown handleEdit={""} handleDelete={""}/>)}
       </Media>
     </div>
   );
