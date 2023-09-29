32462f2d471678d47c02dc97ec94978cea9192f5
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-29 13:51:45 +0200

    Add Profile comp logic and return details

--- frontend/src/pages/profiles/Profile.js
+++ frontend/src/pages/profiles/Profile.js
@@ -1,9 +1,32 @@
 import React from "react";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import Avatar from "../../components/Avatar";
 
-const Profile = () => {
-  return <div></div>;
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner, followers_count } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+        <span className={`mx-2 ${styles.FollowersGrey}`}>{followers_count} followers</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
