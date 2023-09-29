16643f84fe97f572d046c7180c8e4beb248575c8
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-29 14:20:12 +0200

    Add return statement to ProfileDataContext.

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -1,14 +1,14 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
 
-export const ProfileDataContext = createContext();
-export const SetProfileDataContext = createContext();
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
 
 export const useProfileData = () => useContext(ProfileDataContext);
 export const useSetProfileData = () => useContext(SetProfileDataContext);
 
-export const ProfileDataProvider = () => {
+export const ProfileDataProvider = ({ children }) => {
   const [profileData, setProfileData] = useState({
     pageProfile: { results: [] }, // We will use this variable later
     popularProfiles: { results: [] },
@@ -34,8 +34,10 @@ export const ProfileDataProvider = () => {
   }, [currentUser]);
 
   return (
-
-  <div>ProfileDataProvider</div>
-  
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
   );
 };
