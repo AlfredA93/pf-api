0f7d07f86f22fe3328c0bf21f6be72a95e8beea6
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-29 10:43:30 +0200

    Add handleDelete logic to Comment component

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -5,28 +5,57 @@ import Avatar from "../../components/Avatar";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
 
-    const currentUser = useCurrentUser()
-    const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/api/comments/${id}`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
       <hr />
       <Media>
-      <Link to={`/profiles/${profile_id}`}>
-        <Avatar src={profile_image} height={50} />
-      </Link>
-      <Media.Body className="align-self-center ml-2">
         <Link to={`/profiles/${profile_id}`}>
-          <span className={styles.Owner}>{owner}</span>
+          <Avatar src={profile_image} height={50} />
         </Link>
-        <span className={styles.Date}>{updated_at}</span>
-        <p>{content}</p>
-      </Media.Body>
-      {is_owner && (<MoreDropdown handleEdit={""} handleDelete={""}/>)}
+        <Media.Body className="align-self-center ml-2">
+          <Link to={`/profiles/${profile_id}`}>
+            <span className={styles.Owner}>{owner}</span>
+          </Link>
+          <span className={styles.Date}>{updated_at}</span>
+          <p>{content}</p>
+        </Media.Body>
+        {is_owner && <MoreDropdown handleEdit={""} handleDelete={handleDelete} />}
       </Media>
     </div>
   );
