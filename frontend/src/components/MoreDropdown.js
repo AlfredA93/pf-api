ff1caf4a5d680dbf346d1ed6037e3cdf9699d19d
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-01 15:15:11 +0200

    Add modal & logic to ProfileEditDropdown

--- frontend/src/components/MoreDropdown.js
+++ frontend/src/components/MoreDropdown.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { Button, Modal } from "react-bootstrap";
+import axios from "axios";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
@@ -45,32 +47,83 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
 };
 
 export function ProfileEditDropdown({ id }) {
-    const history = useHistory();
-    return (
-      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
-        <Dropdown.Toggle as={ThreeDots} />
-        <Dropdown.Menu>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit`)}
-            aria-label="edit-profile"
-          >
-            <i className="fas fa-edit" /> edit profile
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/username`)}
-            aria-label="edit-username"
-          >
-            <i className="far fa-id-card" />
-            change username
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/password`)}
-            aria-label="edit-password"
-          >
-            <i className="fas fa-key" />
-            change password
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  }
+  const history = useHistory();
+  const [show, setShow] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const { name, email, subject, message } = formData;
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/api/contact/", formData);
+      handleClose();
+    } catch (err) {
+        setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-edit" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
+        </Dropdown.Item>
+        <Dropdown.Item onClick={handleShow} aria-label="ask a question">
+          <i class="fa-regular fa-circle-question" />
+          contact us
+        </Dropdown.Item>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Contact Us</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Please share with us your query and submit below.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleSubmit}>
+              Send
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
