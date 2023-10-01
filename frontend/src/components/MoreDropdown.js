2155c725bff273e2db6816baa974db1e88c62e14
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-01 17:10:30 +0200

    Import Contact comp, refactor modal - MoreDropdown

--- frontend/src/components/MoreDropdown.js
+++ frontend/src/components/MoreDropdown.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
-import btnStyles from "../styles/Button.module.css";
+
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
-import { Alert, Button, Form, Modal } from "react-bootstrap";
-import axios from "axios";
+import Contact from "./Contact";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
@@ -50,35 +49,7 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
 export function ProfileEditDropdown({ id }) {
   const history = useHistory();
   const [show, setShow] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const { name, email, subject, message } = formData;
-  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post("/api/contact/", formData);
-      handleClose();
-    } catch (err) {
-      setErrors(err.response?.data);
-    }
-  };
-
   return (
     <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
       <Dropdown.Toggle as={ThreeDots} />
@@ -107,98 +78,7 @@ export function ProfileEditDropdown({ id }) {
           <i className="fa-regular fa-circle-question" />
           contact us
         </Dropdown.Item>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Contact Us</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              Please let us know what your query is and we'll be in contact with
-              you as soon as possible.
-            </p>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="name">
-                <Form.Label className="d-none">Name</Form.Label>
-                <Form.Control
-                  className={styles.Input}
-                  type="text"
-                  placeholder="Name"
-                  name="name"
-                  value={name}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              {errors.name?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                  {message}
-                </Alert>
-              ))}
-
-              <Form.Group controlId="email">
-                <Form.Label className="d-none">Email</Form.Label>
-                <Form.Control
-                  className={styles.Input}
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  value={email}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              {errors.email?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                  {message}
-                </Alert>
-              ))}
-
-              <Form.Group controlId="subject">
-                <Form.Label className="d-none">Subject</Form.Label>
-                <Form.Control
-                  className={styles.Input}
-                  type="text"
-                  placeholder="Subject"
-                  name="subject"
-                  value={subject}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              {errors.subject?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                  {message}
-                </Alert>
-              ))}
-              <Form.Group controlId="message">
-                <Form.Label className="d-none">Message</Form.Label>
-                <Form.Control
-                  className={styles.Input}
-                  type="textarea"
-                  placeholder="Message"
-                  name="message"
-                  value={message}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              {errors.message?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                  {message}
-                </Alert>
-              ))}
-
-              <Button
-                className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                type="submit"
-              >
-                Submit
-              </Button>
-              {errors.non_field_errors?.map((message, idx) => (
-                <Alert key={idx} variant="warning" className="mt-3">
-                  {message}
-                </Alert>
-              ))}
-            </Form>
-          </Modal.Body>
-        </Modal>
+        <Contact setShow={setShow} show={show} />
       </Dropdown.Menu>
     </Dropdown>
   );
