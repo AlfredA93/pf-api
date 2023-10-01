5cdcb2dfb36d6c76c409d760f281ca909b010129
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-01 10:53:11 +0200

    Add logic to update follow count without refresh

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -18,9 +18,34 @@ export const ProfileDataProvider = ({ children }) => {
 
   const handleFollow = async (clickedProfile) => {
     try {
-        const {data} = await axiosRes.post('/api/followers/', {
-            followed: clickedProfile.id
-        })
+      const { data } = await axiosRes.post("/api/followers/", {
+        followed: clickedProfile.id,
+      });
+      setProfileData((prevState) => ({
+        ...prevState,
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) => {
+            return profile.id === clickedProfile.id
+              ? {
+                  // Profile clicked on. Update its follows count, set following id
+                  ...profile,
+                  follower_count: profile.followers_count + 1,
+                  following_id: data.id,
+                }
+              : profile.is_owner
+              ? {
+                  // Profile of logged in user. Update their following count
+                  ...profile,
+                  following_count: profile.followers_count + 1,
+                }
+              : {
+                  // This is the other profiles in the array, return unchanged.
+                  profile,
+                };
+          }),
+        },
+      }));
     } catch (err) {
       console.log(err);
     }
@@ -45,7 +70,7 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={{setProfileData, handleFollow}}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
