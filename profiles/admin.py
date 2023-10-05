7f83e5523e5de331949c9d5d7fa1d9741c7fd961
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-05 09:55:26 +0200

    Add filters & readonly to admin.py - Profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Admin fields for Post Model"""
+    list_display = ('owner', 'created_at')
+    readonly_fields = ('owner', 'created_at', 'updated_at',
+                       'name', 'bio', 'image')
