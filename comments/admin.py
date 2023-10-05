107b0d435368550054bf919e012cabe93b52fe37
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-05 09:55:03 +0200

    Add filters & readonly to admin.py - Comments

--- comments/admin.py
+++ comments/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Comment
 
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin fields for Post Model"""
+    list_filter = ('owner', 'post')
+    list_display = ('owner', 'content', 'post', 'created_at')
+    readonly_fields = ('owner', 'content', 'post', 'created_at')
