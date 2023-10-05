8d3c289d570bb89a3ec7fc7edfe6280e71fdfd3f
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-10-05 09:55:11 +0200

    Add filters & readonly to admin.py - Contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Message
 
-admin.site.register(Message)
+
+@admin.register(Message)
+class ContactUsAdmin(admin.ModelAdmin):
+    """Admin fields for Post Model"""
+    list_display = ('name', 'email', 'subject', 'created_at')
+    readonly_fields = ('name', 'email', 'subject', 'message', 'created_at')
